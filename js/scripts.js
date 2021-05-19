376a5d385175bbe03a90ad37155e62f3e520e1a5
macOs <macos@macs-MBP.attlocal.net>
2021-05-18 23:14:37 -0700

    3rd push

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,12 @@ jQuery(function() {
 		if (my_switch == false) {my_switch = true;
 				$(this).html('Click for girl');
 				$('p').html('Congratulations on your baby boy!');
-				$('#my_image').attr('src','../images/Baby-boy.png');
+				$('#my_image').attr('src','images/Baby-boy.png');
 		} else {
 			my_switch = false;
 			$(this).html('Click for boy');
 			$('p').html('Congratulations on your baby girl!');
-			$('#my_image').attr('src','../images/Baby-girl.png');
+			$('#my_image').attr('src','images/Baby-girl.png');
 		}
 	});
 });
@@ -19,25 +19,13 @@ jQuery(function() {
 jQuery(function() {
 
 	let change_bg = false;
-	$('#edit-bg').click(function () {
+	$('#edit_bg').click(function () {
 
 		if (change_bg == false) {change_bg = true;
-				$('#my_bg').attr('src','../images/bg-toy.jpg');
+				$('#my_bg').attr('src','images/bg-toy.jpg');
 		} else {
 			change_bg = false;
-			$('#my_bg').attr('src','../images/bg-bird.jpg');
+			$('#my_bg').attr('src','images/bg-bird.jpg');
 		}
 	});
 });
-
-// jQuery(function() {
-// 	$('#edit_background').click(function () {
-//
-// 		if  ( $('.board').hasClass('blue'))
-//
-// 		{ $('.board').removeClass('blue');
-// 	} else {
-// 		$('.board').addClass('blue');}
-//
-// 	});
-// });
